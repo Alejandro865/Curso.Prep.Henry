9e90ab287a919e3883910780be12b75e69ef17e5
Alejandro865 <78055496+Alejandro865@users.noreply.github.com>
2022-01-19 23:23:59 -0500

    fifth commit

---
+++ 03-JS-II/CLASE-01/clase1.js
@@ -0,0 +1,7 @@
+function nombreDeLaFunción(argumento1, argumento2, argumento3) {
+  //codigo de la funciónote
+  return;
+}
+
+nombreDeLaFunción(-1.2, 'hola', true);
+nombreDeLaFunción(1,'hola',true);
